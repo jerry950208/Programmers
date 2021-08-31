73f4b466888fbae471de8681cb6e96c4fa288d6f
jerry950208 <jerry950208@naver.com>
2021-08-31 23:26:44 +0900

    Not solve

---
+++ Programmers_C++/연습문제/최대공약수와_최소공배수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    for(int i=0; i<m; i++){
+        
+    }
+    return answer;
+}
