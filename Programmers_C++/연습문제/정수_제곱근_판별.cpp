a99eed8b1d84cb90dd2a1bd322c5562433fa33fb
jerry950208 <jerry950208@naver.com>
2021-09-01 16:39:56 +0900

    Solve

---
+++ Programmers_C++/연습문제/정수_제곱근_판별.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <cmath>
+using namespace std;
+
+long long solution(long long n){
+	long long answer = 0;
+	
+	if(sqrt(n)-(int)sqrt(n) != 0) answer = -1;
+	
+	else answer = pow(sqrt(n)+1,2);
+
+	return answer;
+}
