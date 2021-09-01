471604c217003bea5c9d5f79709b5b6c010b7065
jerry950208 <jerry950208@naver.com>
2021-09-01 15:50:01 +0900

    Solve

--- Programmers_C++/연습문제/최대공약수와_최소공배수.cpp
+++ Programmers_C++/연습문제/최대공약수와_최소공배수.cpp
@@ -5,8 +5,29 @@ using namespace std;
 
 vector<int> solution(int n, int m) {
     vector<int> answer;
-    for(int i=0; i<m; i++){
-        
+    int tmp = 0;
+    for(int i=1; i<=m; i++){
+        if(n % i == 0 && m % i == 0){
+            tmp = i;
+        }
+    }
+    if(tmp == 0)
+        answer.push_back(1);
+    else
+    	answer.push_back(tmp);
+    int cnt = 1;
+    tmp = m;
+    while(1){
+        if(n * cnt == tmp){
+            answer.push_back(tmp);
+            break;
+        }
+        else if(n * cnt > tmp){
+            tmp += m;
+            cnt = 1;
+        }
+        else
+            cnt++;
     }
     return answer;
 }
