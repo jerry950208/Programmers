ba1aa56ed58959a9a5512c7ec6741d1081fe2a25
jerry950208 <jerry950208@naver.com>
2021-09-07 15:20:46 +0900

    Solve

---
+++ Programmers_C++/Level 2/연습문제/최솟값_만들기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B){
+    int answer = 0;
+    
+    sort(A.begin(), A.end());	// 오름차순
+    sort(B.rbegin(), B.rend());		//내림차순
+    
+	for(int i=0; i<A.size(); i++)
+    	answer += A[i] * B[i];
+
+    return answer;
+}
