67ecc76e67d85da3d13d31a0a126091d05c56441
jerry950208 <jerry950208@naver.com>
2021-09-09 22:28:34 +0900

    Solve

---
+++ Programmers_C++/Level 2/연습문제/다음_큰_숫자.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int countOne(int n){
+    int ret = 0;
+    
+    for(int i=0; i<31; i++)
+        if(n & 0x01<<i)		// 이진수 n의 자리마다 1을 검사하는 코드
+            ret++;
+    
+    return ret;
+}
+
+int solution(int n) {
+    int answer = countOne(n);
+    
+    while(1){
+        n++;
+        if(answer == countOne(n))
+            return n;
+    }
+}
