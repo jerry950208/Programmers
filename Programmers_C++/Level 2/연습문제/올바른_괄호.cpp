772bdbe30021f90d9d74a6fbea16e41b635806d7
jerry950208 <jerry950208@naver.com>
2021-09-10 22:35:03 +0900

    Study

--- Programmers_C++/Level 2/연습문제/올바른_괄호.cpp
+++ Programmers_C++/Level 2/연습문제/올바른_괄호.cpp
@@ -26,3 +26,28 @@ bool solution(string s)
     
     return answer;
 }
+
+//-----------------------------------------------
+//stack 사용을 안했을경우
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s){
+	
+	int n = 0;
+
+	for(int i=0; i<s.length(); i++){
+		
+		if(n < 0)
+			return false;
+			
+		if(s[i] == '(')
+			n++;
+		
+		else if(s[i] == ')')
+			n--;
+	}
+
+	return n == 0; 			//여기서 n == 0의 의미는 n이 0일경우 true를 반환, 아닐경우 false를 반환한다
+}
