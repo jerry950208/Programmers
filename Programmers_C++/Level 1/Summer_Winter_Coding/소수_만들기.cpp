6e73e592968b3c9cc65494e1d0cb5d5010f79dc9
jerry950208 <jerry950208@naver.com>
2021-09-11 17:34:13 +0900

    Solve

---
+++ Programmers_C++/Level 1/Summer_Winter_Coding/소수_만들기.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool func_check(long long num){
+    if(num == 0 || num == 1)
+        return false;
+    
+    for(int i=2; i<num/2; i++)
+        if(num % i == 0)
+            return false;
+    return true;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+	
+    for(int i=0; i<nums.size(); i++){
+        for(int j=i + 1; j<nums.size(); j++){
+            for(int k=j + 1; k<nums.size(); k++){
+                int n = nums[i] + nums[j] + nums[k];
+                if(func_check(n) == true)
+                    answer++;
+            }
+        }
+    }
+
+    return answer;
+}
--- Programmers_C++/Level 1/카카오/다트_게임.cpp
+++ Programmers_C++/Level 1/카카오/다트_게임.cpp
@@ -1,25 +1,105 @@
 #include <iostream>
+
 #include <string>
 #include <cmath>
 using namespace std;
 
-int solution(string dartResult){
-	int answer = 0, secore;
+string s;
+int answer, tmp;
+
+int func_S(int i, int num){
 	
-	for(int i=0; i<dartResult.size(); i++){
-		
-		if(dartResult[i] == 'S' || dartResult[i] == 'D' || dartResult[i] == 'T'){
-			if(dartResult[i] == 'S')
-				score = 
-		}
+	int score = num;
+	
+	if(s[i + 2] == '*'){
+		score *= 2;
+		if(s[i + 5] == '*' || s[i - 1] == '*')
+			score *= 2;
 	}
 
-	return 0;
+	else if(s[i + 2] == '#'){
+		score *= -1;
+		if(s[i + 5] == '*')
+			score *= 2;
+	}
+
+	return score;
 }
 
-int main(void){
+int func_D(int i, int num){
 	
-	string s;
-	cin >> s;
-	cout << solution(s) << endl;
+	int score = pow(num, 2);
+	
+	if(s[i + 2] == '*'){
+		score *= 2;
+		if(s[i + 5] == '*' || s[i - 1] == '*')
+			score *= 2;
+	}
+
+	else if(s[i + 2] == '#'){
+		score *= -1;
+		if(s[i + 5] == '*')
+			score *= 2;
+	}
+
+	return score;
+}
+
+int func_T(int i, int num){
+	int score = pow(num, 3);
+
+	if(s[i + 2] == '*'){
+		score *= 2;
+		if(s[i + 5] == '*' || s[i - 1] == '*')
+			score *= 2;
+	}
+
+	else if(s[i + 2] == '#'){
+		score *= -1;
+		if(s[i + 5] == '*')
+			score *= 2;
+	}
+
+	return score;
+}
+
+int solution(string dartResult) {
+    
+	s = dartResult;
+	
+	for(int i=0; i<dartResult.length(); i++){
+		
+		if(isdigit(dartResult[i])){
+			
+			if(!isdigit(dartResult[i + 1])){
+				if(dartResult[i + 1] == 'S')
+					answer += func_S(i, dartResult[i] - '0');
+
+				else if(dartResult[i + 1] == 'D')
+					answer += func_D(i, dartResult[i] - '0');
+			
+				else if(dartResult[i + 1] == 'T')
+					answer += func_T(i, dartResult[i] - '0');
+			}
+			else{
+
+				if(dartResult[i + 1] == 'S')
+					answer += func_S(i, 10);
+
+				else if(dartResult[i + 1] == 'D')
+					answer += func_D(i, 10);
+			
+				else if(dartResult[i + 1] == 'T')
+					answer += func_T(i, 10);
+			}
+		}
+		cout << answer << endl;
+	}
+    return answer;
+}
+int main(void){
+	string input;
+	cin >> input;
+	cout << solution(input) << endl;
+	return 0;
 }
