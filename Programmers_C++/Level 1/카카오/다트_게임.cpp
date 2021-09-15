4493ab3d115a382e869a01aafc2e4587a9ab5cc4
jerry950208 <jerry950208@naver.com>
2021-09-15 22:52:23 +0900

    NOT SOLVE

--- Programmers_C++/Level 1/카카오/다트_게임.cpp
+++ Programmers_C++/Level 1/카카오/다트_게임.cpp
@@ -1,132 +1,65 @@
-#include <iosteam>
-#include <string>
-#include <cmath>
-using namespace std;
-
-int solution(string dartResult) {
-    int answer = 0;
-    
-	for(int i=0; i<dartResult.size(); i++){
-		
-		if(dartResult[i] == 'S')
-
-		else if(dartResult[i] == 'D')
-
-		else if(dartResult[i] == 'T')
-	}
-	return answer;
-}
-
-int main(void){
-
-	string s;
-	cin >> s;
-
-	cout << solution(s) << endl;
-	return 0;
-}
 /*
-#include <iostream>
+1. 다트 게임은 총 3번의 기회로 구성된다.
+
+2. 각 기회마다 얻을 수 있는 점수는 0점에서 10점까지이다.
+
+3. 점수와 함께 Single(S), Double(D), Triple(T) 영역이 존재하고 각 영역 당첨 시 점수에서 1제곱, 2제곱, 3제곱 (점수1 , 점수2 , 점수3 )으로 계산된다.
+
+4. 옵션으로 스타상(*) , 아차상(#)이 존재하며 스타상(*) 당첨 시 해당 점수와 바로 전에 얻은 점수를 각 2배로 만든다. 아차상(#) 당첨 시 해당 점수는 마이너스된다.
+
+5. 스타상(*)은 첫 번째 기회에서도 나올 수 있다. 이 경우 첫 번째 스타상(*)의 점수만 2배가 된다. (예제 4번 참고)
+
+6. 스타상(*)의 효과는 다른 스타상(*)의 효과와 중첩될 수 있다. 이 경우 중첩된 스타상(*) 점수는 4배가 된다. (예제 4번 참고)
+
+7. 스타상(*)의 효과는 아차상(#)의 효과와 중첩될 수 있다. 이 경우 중첩된 아차상(#)의 점수는 -2배가 된다. (예제 5번 참고)
+
+8. Single(S), Double(D), Triple(T)은 점수마다 하나씩 존재한다.
+
+9. 스타상(*), 아차상(#)은 점수마다 둘 중 하나만 존재할 수 있으며, 존재하지 않을 수도 있다.
+
+0~10의 정수와 문자 S, D, T, *, #로 구성된 문자열이 입력될 시 총점수를 반환하는 함수를 작성하라.
+*/
 #include <string>
 #include <cmath>
 using namespace std;
 
-string s;
-int answer, tmp;
-
-int func_S(int i, int num){
-	
-	int score = num;
-	
-	if(s[i + 2] == '*'){
-		score *= 2;
-		if(s[i + 5] == '*' || s[i - 1] == '*')
-			score *= 2;
-	}
-
-	else if(s[i + 2] == '#'){
-		score *= -1;
-		if(s[i + 5] == '*')
-			score *= 2;
-	}
-
-	return score;
-}
-
-int func_D(int i, int num){
-	
-	int score = pow(num, 2);
-	
-	if(s[i + 2] == '*'){
-		score *= 2;
-		if(s[i + 5] == '*' || s[i - 1] == '*')
-			score *= 2;
-	}
-
-	else if(s[i + 2] == '#'){
-		score *= -1;
-		if(s[i + 5] == '*')
-			score *= 2;
-	}
-
-	return score;
-}
-
-int func_T(int i, int num){
-	int score = pow(num, 3);
-
-	if(s[i + 2] == '*'){
-		score *= 2;
-		if(s[i + 5] == '*' || s[i - 1] == '*')
-			score *= 2;
-	}
-
-	else if(s[i + 2] == '#'){
-		score *= -1;
-		if(s[i + 5] == '*')
-			score *= 2;
-	}
-
-	return score;
-}
-
 int solution(string dartResult) {
     
-	s = dartResult;
-	
-	for(int i=0; i<dartResult.length(); i++){
-		
-		if(isdigit(dartResult[i])){
-			
-			if(!isdigit(dartResult[i + 1])){
-				if(dartResult[i + 1] == 'S')
-					answer += func_S(i, dartResult[i] - '0');
-
-				else if(dartResult[i + 1] == 'D')
-					answer += func_D(i, dartResult[i] - '0');
-			
-				else if(dartResult[i + 1] == 'T')
-					answer += func_T(i, dartResult[i] - '0');
-			}
-			else{
-
-				if(dartResult[i + 1] == 'S')
-					answer += func_S(i, 10);
-
-				else if(dartResult[i + 1] == 'D')
-					answer += func_D(i, 10);
-			
-				else if(dartResult[i + 1] == 'T')
-					answer += func_T(i, 10);
-			}
-		}
-		cout << answer << endl;
-	}
+    int answer = 0, score[3] = { 0 }, idx = 0;
+    
+    for(int i=0; i<dartResult.size(); i++){
+        
+        if(dartResult[i] == 'S'){}
+        
+        else if(dartResult[i] == 'D')
+            score[idx - 1] = pow(score[idx - 1], 2);
+        
+        else if(dartResult[i] == 'T')
+            score[idx - 1] = pow(score[idx - 1], 3);
+        
+        else if(dartResult[i] == '*'){
+            score[idx - 1] *= 2;
+            if(idx > 1)
+                score[idx - 2] *= 2;
+        }
+            
+        else if(dartResult[i] == '#')
+            score[idx - 1] *= -1;
+        
+        else{
+            if(dartResult[i + 1] == '0'){
+                score[idx] = 10;
+                i++;
+            }
+            else{
+                score[idx] = dartResult[i] - '0'; 
+                idx++;
+            }
+        }
+    }
+    
+    for(int i=0; i<3; i++)
+        answer += score[i];
+    
     return answer;
 }
-int main(void){
-	string input;
-	cin >> input;
-	cout << solution(input) << endl;
-	return 0;
-}*/
