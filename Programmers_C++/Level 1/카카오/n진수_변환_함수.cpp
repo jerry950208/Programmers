a92c1a25a67b58ffefda71dbad24e350721aa03b
jerry950208 <jerry950208@naver.com>
2021-09-17 22:49:54 +0900

    NOT SOLVE

---
+++ Programmers_C++/Level 1/카카오/n진수_변환_함수.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> func_number(int change_num, int n){
+	vectorr<int> output;
+	while(n > 1){
+		
+	}
+
+	return output;
+}
+
+int main(void){
+	int n;
+	cin >> n;
+	cout << func_number(2, n);
+	return 0;
+}
--- Programmers_C++/Level 1/카카오/비밀지도.cpp
+++ Programmers_C++/Level 1/카카오/비밀지도.cpp
@@ -1,3 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    return answer;
+}
+/*
 #include <iostream>
 #include <string>
 #include <vector>
@@ -14,4 +24,4 @@ int main(void){
 		cout << flag[i] << endl;
 	cout << true << " " << false << endl;
 	return 0;
-}
+}*/
