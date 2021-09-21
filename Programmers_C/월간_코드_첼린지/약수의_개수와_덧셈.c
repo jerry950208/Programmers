c088fba5ed5f8a56f5edacd759c678c7839ceb2b
jerry950208 <jerry950208@naver.com>
2021-09-21 20:17:50 +0900

    Other Solution

--- Programmers_C/월간_코드_첼린지/약수의_개수와_덧셈.c
+++ Programmers_C/월간_코드_첼린지/약수의_개수와_덧셈.c
@@ -17,3 +17,20 @@ int solution(int left, int right) {
     }
     return answer;
 }
+
+/*
+//다른사람 풀이
+int sign(int n){
+	int cnt = 1;
+	for(int i=1, last=n >> 1; i<=last ++i)
+		if(n % i == 0)
+			++cnt;
+
+	return cnt & 1 ? -1 : 1;
+}
+
+int solution(int a, int b){
+	
+	return a > b ? 0 : sign(a) * a + solution(a+1, b);
+}
+*/
