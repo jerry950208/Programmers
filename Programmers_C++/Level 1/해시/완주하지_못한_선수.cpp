1721be09d9b9994ab1687107f9f10de27afdf302
jerry950208 <jerry950208@naver.com>
2021-09-28 23:30:47 +0900

    Solve

--- Programmers_C++/Level 1/해시/완주하지_못한_선수.cpp
+++ Programmers_C++/Level 1/해시/완주하지_못한_선수.cpp
@@ -1,24 +1,13 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
-string solution(vector<string> participant, vector<string> completion) {
-    string answer = "";
-    for(int i=0; i<participant.size(); i++){
-        for(int j=0; j<completion.size(); j++){
-            if(participant[i] == completion[j]){
-				participant.erase(participant.begin() + i);
-                break;
-            }
-        }
-    }
+string solution(vector<string> participant, vector<string> completion){
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
     
-    //for(int i=0; i<participant.size(); i++){
-    //    if(participant[i] != NULL)
-    //        answer += participant[i];
-    //}
-    
-    answer = participant;
-    return answer;
+    for(int i=0; i<participant.size(); i++)
+        if(participant[i] != completion[i])
+            return participant[i];
 }
