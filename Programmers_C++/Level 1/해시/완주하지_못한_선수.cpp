30b77523784fe71575e0a2c0de14d8d48a55c51d
jerry950208 <jerry950208@naver.com>
2021-09-28 23:33:53 +0900

    Other Solution

--- Programmers_C++/Level 1/해시/완주하지_못한_선수.cpp
+++ Programmers_C++/Level 1/해시/완주하지_못한_선수.cpp
@@ -11,3 +11,26 @@ string solution(vector<string> participant, vector<string> completion){
         if(participant[i] != completion[i])
             return participant[i];
 }
+
+/*
+//다른사람의 풀이
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion){
+   
+    map<string, int> m;
+    
+    for(auto c : completion)
+        m[c] += 1;
+    
+    for(auto p : participant){
+        m[p] -= 1;
+        if(m[p] < 0)
+            return p;
+    }
+    
+}*/
+*/
