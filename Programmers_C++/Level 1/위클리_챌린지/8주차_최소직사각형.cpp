aa1676ee01f98e5238c24dff056d94b7c61c9316
jerry950208 <jerry950208@naver.com>
2021-09-29 22:38:01 +0900

    Other Solution

--- Programmers_C++/Level 1/위클리_챌린지/8주차_최소직사각형.cpp
+++ Programmers_C++/Level 1/위클리_챌린지/8주차_최소직사각형.cpp
@@ -22,3 +22,25 @@ int solution(vector<vector<int>> sizes) {
     answer = max * sizes[sizes.size()-1][0];
     return answer;
 }
+
+/*
+//다른사람의 풀이
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(vector<vector<int> > sizes){
+	int answer = 0;
+
+	int w=0, h = 0;
+	for(int i=0; i<sizes.size(); i++){
+		w = max(w, min(sizes[i][0], sizes[i][1]));
+		h = max(h, max(sizes[i][0], sizes[i][1]));
+	}
+
+	answer = w * h;
+
+	return answer;
+
+}
+*/
