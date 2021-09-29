16f4d0fc18f3bd21510a56da574f4c32e529259f
jerry950208 <jerry950208@naver.com>
2021-09-29 22:32:34 +0900

    Solve

--- Programmers_C++/Level 1/위클리_챌린지/8주차_최소직사각형.cpp
+++ Programmers_C++/Level 1/위클리_챌린지/8주차_최소직사각형.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 int solution(vector<vector<int>> sizes) {
     int answer = 0, max = 0;
+    
     for(int i=0; i<sizes.size(); i++){
-    	if(sizes[i][0] < sizes[i][1]){
+        if(sizes[i][1] < sizes[i][0]){
             int tmp = sizes[i][0];
             sizes[i][0] = sizes[i][1];
             sizes[i][1] = tmp;
         }
         
-        if(max < sizes[i][0])
-            max = sizes[i][0];
+        if(max < sizes[i][1])
+            max = sizes[i][1];
+        
     }
-    
     sort(sizes.begin(), sizes.end());
     
-    answer += max * sizes[0][0];
-    
+    answer = max * sizes[sizes.size()-1][0];
     return answer;
 }
