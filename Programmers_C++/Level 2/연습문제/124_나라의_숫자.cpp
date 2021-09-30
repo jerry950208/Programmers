0eebee1650d644d2c4138ee60d58dfefec216867
jerry950208 <jerry950208@naver.com>
2021-09-30 22:58:59 +0900

    Solve

--- Programmers_C++/Level 2/연습문제/124_나라의_숫자.cpp
+++ Programmers_C++/Level 2/연습문제/124_나라의_숫자.cpp
@@ -1,28 +1,20 @@
 #include <string>
 #include <vector>
-#include <list>
-#include <deque>
+
 using namespace std;
 
-deque solution(int n) {
-    //int answer;
-    deque <int> dq;
-    while(n < 0){
+string solution(int n) {
+    string answer = "", tmp[3] = {"4", "1", "2"};
+    
+    while(n){
+        answer = tmp[n % 3] + answer;
         
-        if(n % 3 == 1)
-            dq.push_front(1);
-        
-        else if(n % 3 == 2)
-            dq.push_front(2);
+        if(!(n % 3))
+            n = n / 3 - 1;
         
         else
-            dq.push_front(4);
-        
-        n /= 3;
+            n = n / 3;
     }
     
-    answer = dq;
-    
-    return dq;
-    
+    return answer;
 }
