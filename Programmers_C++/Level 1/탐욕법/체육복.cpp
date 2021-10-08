a73d54384e08e73cb23e84ca6f7dfb47d6e10e50
jerry950208 <jerry950208@naver.com>
2021-10-08 18:47:03 +0900

    Not Solve

---
+++ Programmers_C++/Level 1/탐욕법/체육복.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = n - lost.size();
+    vector<int> own(n+1,1);
+    int check[n+1];
+    
+    for(int i=0; i<n+1; i++)
+        check[i] = 0;
+        
+    for(int i=0; i<lost.size(); i++){
+        own[lost[i]] = 0;
+     	check[lost[i]] = 1;
+    }
+    
+    for(int i=0; i<reserve.size(); i++){
+        if(check[reserve[i]] == 1)
+            own[reserve[i]] = 1;
+        else
+        	own[reserve[i]] = 2;
+        
+    }
+    
+    for(int i=0; i<n; i++){
+        if(own[i] == 0 && own[i-1] == 2){
+            answer++;
+            own[i] = 1;
+            own[i-1] = 1;
+        }
+        
+        else if(own[i] == 0 && own[i+1] == 2){
+            answer++;
+            own[i] = 1;
+            own[i+1] = 1;
+        }
+		
+        else
+            continue;
+    }
+    return answer;
+}
