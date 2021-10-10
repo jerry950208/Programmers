4f1065b4607448f991a0d6e033e0769ffa14632d
jerry950208 <jerry950208@naver.com>
2021-10-10 10:13:06 +0900

    Not Solve

--- Programmers_C++/Level 1/탐욕법/체육복.cpp
+++ Programmers_C++/Level 1/탐욕법/체육복.cpp
@@ -1,23 +1,23 @@
 #include <string>
 #include <vector>
-#include <iosteam>
+
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = n - lost.size();
     vector<int> own(n+1,1);
-    int check[n+1];
+    int lost_check[n+1];
     
     for(int i=0; i<n+1; i++)
-        check[i] = 0;
+        lost_check[i] = 0;
         
     for(int i=0; i<lost.size(); i++){
         own[lost[i]] = 0;
-     	check[lost[i]] = 1;
+     	lost_check[lost[i]] = 1;
     }
     
     for(int i=0; i<reserve.size(); i++){
-        if(check[reserve[i]] == 1)
+        if(lost_check[reserve[i]] == 1)
             own[reserve[i]] = 1;
         else
         	own[reserve[i]] = 2;
@@ -25,13 +25,13 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     }
     
     for(int i=0; i<n; i++){
-        if(own[i] == 0 && own[i-1] == 2){
+        if(own[i] == 0 && own[i-1] == 2 && i != 0){
             answer++;
             own[i] = 1;
             own[i-1] = 1;
         }
         
-        else if(own[i] == 0 && own[i+1] == 2){
+        else if(own[i] == 0 && own[i+1] == 2 && i != n-1){
             answer++;
             own[i] = 1;
             own[i+1] = 1;
@@ -41,18 +41,4 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
             continue;
     }
     return answer;
-}
-
-int main(void){
-	
-	vector<int> lost;
-	vector<int> reserve;
-	int n;
-
-	cin >> n;
-	lost.push_bakc(n);
-	
-	cin >> n;
-	reserve.push_back(n);
-	
 }
