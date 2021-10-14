b1f9313d5d1ef261d10002e28b462a1d5811c999
jerry950208 <jerry950208@naver.com>
2021-10-14 21:10:19 +0900

    Solve

--- Programmers_C++/Level 2/연습문제/땅따먹기.cpp
+++ Programmers_C++/Level 2/연습문제/땅따먹기.cpp
@@ -1,63 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 int solution(vector<vector<int> > land)
 {
     int answer = 0;
-    int size = land.size();
-    int tmp[4][size];
-    
-    for(int i=0; i<size; i++){
-		tmp[0][i] = land[0][i];
-        cout << tmp[0][i] << " ";
-	}
-	cout << endl;
-
-    for(int i=1; i<3; i++){
-        for(int j=0; j<size; j++){
-            if(j == 0){
-                tmp[i][j] = tmp[i-1][j] + land[i][j+1];
-            	cout << tmp[i][j] << " ";
-			}
-
-            else if(j == size - 1){
-                tmp[i][j] = tmp[i-1][j] + land[i][j-1];
-            	cout << tmp[i][j] << " ";
-			}
-
-            else{
-                tmp[i][j] = tmp[i-1][j] + max(land[i][j-1], land[i][j+1]);
-        		cout << tmp[i][j] << " ";
-			}
-		}
-		cout << endl;
+    for(int i = 0; i < land.size()-1 ; i++){
+        land[i+1][0] += max(land[i][1],max(land[i][2],land[i][3]));
+        land[i+1][1] += max(land[i][0],max(land[i][2],land[i][3]));
+        land[i+1][2] += max(land[i][0],max(land[i][1],land[i][3]));
+        land[i+1][3] += max(land[i][0],max(land[i][1],land[i][2]));
     }
-    
-    for(int i=0; i<size; i++)
-        if(answer < tmp[3][i])
-            answer = tmp[3][i];
-        //answer = max(answer, tmp[3][i]);
-    
+    answer = max(land[land.size()-1][0], max(land[land.size()-1][1], max( land[land.size()-1][2], land[land.size()-1][3] )));
     return answer;
-}
-
-int main(void){
-	
-	vector<vector<int> > v;
-	vector<int> tmp;
-	int input;
-
-	for(int i=0; i<4; i++){
-		for(int j=0; j<3; j++){
-			cin >> input;
-			tmp.push_back(input);
-		}
-		v.push_back(tmp);
-	}
-
-	cout << solution(v) << endl;
-
-	return 0;
 }
