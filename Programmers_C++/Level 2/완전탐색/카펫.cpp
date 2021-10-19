954a29544d1e56f31d7597a2169049dac676572f
jerry950208 <jerry950208@naver.com>
2021-10-19 22:13:09 +0900

    Solve

--- Programmers_C++/Level 2/완전탐색/카펫.cpp
+++ Programmers_C++/Level 2/완전탐색/카펫.cpp
@@ -1,20 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
-    int sum = brown + yellow;
-    int flag = sum;
     
-    for(int i=1; i<=brown; i++){
-        for(int j=1; j<=brown; j++){
-            if(i*j == sum && max(i,j) - min(i,j) < flag){
-                answer.erase(answer.begin(), answer.end());
-                answer.push_back(j);
-                answer.push_back(i);
-                flag = max(i,j) - min(i,j);
+    int sum = brown + yellow;
+    
+    for(int i=sum/2; i>0; i--){
+		if(sum % i == 0){
+            int a = i;
+            int b = sum / i;
+            
+            if((a-2) * (b-2) == yellow){
+                answer.push_back(a);
+                answer.push_back(b);
+                break;
             }
         }
     }
