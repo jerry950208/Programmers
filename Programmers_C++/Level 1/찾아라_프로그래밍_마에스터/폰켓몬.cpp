136c3cc9b84f54712646382966c7accc6a2982ae
jerry950208 <jerry950208@naver.com>
2021-10-21 22:18:54 +0900

    Solve

--- Programmers_C++/Level 1/찾아라_프로그래밍_마에스터/폰켓몬.cpp
+++ Programmers_C++/Level 1/찾아라_프로그래밍_마에스터/폰켓몬.cpp
@@ -1,10 +1,8 @@
-#include <vector>
-using namespace std;
 
 int solution(vector<int> nums)
 {
     int answer = 0;
-    bool flag[20001];
+    bool flag[210000];
     int cnt = 0;
     for(int i=0; i<nums.size(); i++){
         if(flag[nums[i]] == false){
@@ -19,3 +17,21 @@ int solution(vector<int> nums)
         answer = nums.size() / 2;
     return answer;
 }
+
+//다른사람들 풀이
+/*
+//unordered_map 따로 공부해보기
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int solution(vector<int> nums){
+	
+	unordered_map<int, int> hash;
+
+	for(auto num: nums)
+		hash[num] += 1;
+
+		return min(hash.size(), nums.size()/2);
+}
+*/
