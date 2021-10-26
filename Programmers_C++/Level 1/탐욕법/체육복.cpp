e63569350b5521794063b50e2421c77f48014ade
jerry950208 <jerry950208@naver.com>
2021-10-26 23:18:00 +0900

    Not Solve

--- Programmers_C++/Level 1/탐욕법/체육복.cpp
+++ Programmers_C++/Level 1/탐욕법/체육복.cpp
@@ -5,27 +5,34 @@ using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
-    int cnt = 0;
-    bool flag[n];
+    int cnt;
+    bool flag[n+1];
     
-    for(int i=0; i<reserve.size(); i++)
+    if(n == 0 || n == 1){
+        return n;
+    }
+    
+    for(int i=0; i<n; i++){
+        flag[i] = false;
+    }
+    
+    for(int i=0; i<reserve.size(); i++){
         flag[reserve[i]] = true;
-	
+    }
+    
     for(int i=0; i<lost.size(); i++){
-        
-        if(flag[lost[i]] == true)
-            flag[lost[i]] = false;
-        
-        else if(flag[lost[i]-1] == true){
+        if(flag[lost[i] - 1] == true){
+            flag[lost[i] - 1] = false;
             cnt++;
-            flag[lost[i]-1] = false;
         }
-        else if(flag[lost[i]+1] == true){
+        
+        else if(flag[lost[i] + 1] == true){
+            flag[lost[i] + 1] = false;
             cnt++;
-            flag[lost[i]+1] = false;
         }
     }
     
-    answer = n - lost.size() + cnt;
+    answer = n - (lost.size() - cnt);
+    
     return answer;
 }
